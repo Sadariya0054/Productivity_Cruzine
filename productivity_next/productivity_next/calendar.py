ae93c48f649f0ee18aa97baa189ed252b73947d3
Finbyz Tech <info.finbyz.com>
2024-12-23 16:12:03 +0530

    MULTIPLE CHANGES MD

--- productivity_next/productivity_next/calendar.py
+++ productivity_next/productivity_next/calendar.py
@@ -19,7 +19,8 @@ def get_custom_events(doctype, start=None, end=None, filters=None):
             if len(filters) > 0 and len(filters[0]) == 5:
                 field = filters[0][1]
                 value = filters[0][3]
-                conditions.append(f"`{field}` = '{value}'")
+                conditions.append(f"`{field}` BETWEEN '{value[0]}' AND '{value[1]}'")
+
             elif len(filters) > 0 and len(filters[0]) == 3:
                 field = filters[0][0]
                 value = filters[0][2]
@@ -39,9 +40,12 @@ def get_custom_events(doctype, start=None, end=None, filters=None):
         if event.get('task_owner'):
             try:
                 first_name = event['task_owner'].split('@')[0].split('.')[0].capitalize()
-                event['title'] = f"{event.get('subject', '')} ({first_name})"
+                event['title'] = f"({first_name}) {event.get('subject', '')} "
             except Exception as e:
-                event['title'] = f"{event.get('subject', '')} ({event.get('task_owner', 'No Owner')})"
+                event['title'] = f"({event.get('task_owner', 'No Owner')}) {event.get('subject', '')} ({event.get('task_owner', 'No Owner')})"
         else:
             event['title'] = f"{event.get('subject')}"
+
+        if event.get('completed_on'):
+            event['exp_start_date'] = event.get('completed_on')
     return events
